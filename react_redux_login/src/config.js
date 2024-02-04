2f8d9a732c2fbf7188007aad7e8ebae418bb1486
Tulasi Joshua Mbawini <mbawinitulasi12@gmail.com>
2024-02-04 23:36:34 +0000

    Feat: setting api

---
+++ react_redux_login/src/config.js
@@ -0,0 +1,3 @@
+const hostName = 'https://cointossapi.pythonanywhere.com/'
+
+export default hostName;
--- react_redux_login/src/pages/Login.jsx
+++ react_redux_login/src/pages/Login.jsx
@@ -1,19 +1,27 @@
 import React, { useState } from 'react'
 import { useDispatch } from 'react-redux';
 import { loginUser } from '../store/UserSlice';
+import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
 
     const dispatch = useDispatch();
+    const navigate = useNavigate();
 
     const handleLoginEvent = (e) => {
         e.preventDefault();
         let userCredentials={
             email, password
         }
-        dispatch(loginUser(userCredentials))
+        dispatch(loginUser(userCredentials)).then((result) => {
+            if(result.payload) {
+                setEmail('');
+                setPassword('');
+                navigate('/');
+            }
+        })
     }
   return (
     <form className='form-group custom-form' onSubmit={handleLoginEvent}>
