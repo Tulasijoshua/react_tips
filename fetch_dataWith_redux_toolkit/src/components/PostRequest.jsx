281c04519c5d96a53e983a4f10b04743867e5506
Tulasi Joshua Mbawini <mbawinitulasi12@gmail.com>
2024-02-09 23:34:38 +0000

    Feat: redux

---
+++ fetch_dataWith_redux_toolkit/src/components/PostRequest.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { useDispatch } from 'react-redux'
+
+const PostRequest = () => {
+    const dispatch = useDispatch()
+    
+  return (
+    <div>PostRequest</div>
+  )
+}
+
+export default PostRequest
