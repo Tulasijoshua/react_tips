cdfd1f0ffa3a88150802955a4e28d229f772fb0a
Tulasi Joshua Mbawini <mbawinitulasi12@gmail.com>
2024-02-09 23:37:18 +0000

    Feat: update

--- fetch_dataWith_redux_toolkit/src/components/PostRequest.jsx
+++ fetch_dataWith_redux_toolkit/src/components/PostRequest.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
-import { useDispatch } from 'react-redux'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { getPosts } from '../redux/feature/postSlice'
 
 const PostRequest = () => {
     const dispatch = useDispatch()
-    
+    const {posts, loading} = useSelector((state) => state.post)
+
+    useEffect(() => {
+        dispatch(getPosts())
+    }, [])
+
   return (
-    <div>PostRequest</div>
+    <div>
+        {posts.map((item, index) => (
+            <h2>{item.title}</h2>
+        ))}
+    </div>
   )
 }
 
