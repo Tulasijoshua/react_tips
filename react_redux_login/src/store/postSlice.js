dd2a27739c9b07a5f67c8369ac5ea12e987f002c
Tulasi Joshua Mbawini <mbawinitulasi12@gmail.com>
2024-02-10 00:25:03 +0000

    Feat: post redux

--- react_redux_login/src/store/postSlice.js
+++ react_redux_login/src/store/postSlice.js
@@ -1,13 +1,35 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import axios from "axios";
 
-const postSlice = createSlice({
-    name: 'post',
+const initialState = {
+    loading: false,
     posts: [],
     error: '',
-})
+}
 
 export const fetchPosts = createAsyncThunk('post/fetchPosts', () => {
     return axios
         .get('https://jsonplaceholder.typicode.com/users')
+        .then((res) => res.data)
+})
+
+const postSlice = createSlice({
+    name: 'post',
+    initialState,
+    extraReducers: (builder) => {
+        builder
+        .addCase(fetchPosts.pending, (state) => {
+            state.loading = true;
+        })
+        .addCase(fetchPosts.fulfilled, (state, action) => {
+            state.loading = false;
+            state.posts = action.payload;
+            state.error = ''
+        })
+        .addCase(fetchPosts.rejected, (state, action) => {
+            state.loading = false;
+            state.posts = [];
+            state.error = action.error.message;
+        })
+    }
 })
