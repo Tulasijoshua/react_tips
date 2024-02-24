0d49c80b996e773f15b0df7b3f25eeb8893b5b9b
Tulasi Joshua Mbawini <mbawinitulasi12@gmail.com>
2024-02-24 21:15:54 +0000

    Feat: update

--- react_lazy_loading/src/components/ImgProfile.jsx
+++ react_lazy_loading/src/components/ImgProfile.jsx
@@ -1,7 +1,27 @@
-import React, { useState } from 'react'
+import React, { useCallback, useState } from 'react'
 
 const ImgProfile = ({ path, placeholder, loadError, ...props }) => {
     const [img, initImg] = useState(placeholder || path);
+
+    const onLoad = useCallback(() => {
+        initImg(path);
+      }, [path]);
+
+      const onError = useCallback(() => {
+        initImg(loadError || placeholder);
+      }, [loadError, placeholder]);
+
+      useEffect(() => {
+        const imageObjt = new Image();
+        imageObjt.src = path;
+        imageObjt.addEventListener("load", onLoad);
+        imageObjt.addEventListener("error", onError);
+        return () => {
+          imageObjt.removeEventListener("load", onLoad);
+          imageObjt.removeEventListener("error", onError);
+        };
+      }, [path, onLoad, onError]);
+      
   return (
     <div>ImgProfile</div>
   )
