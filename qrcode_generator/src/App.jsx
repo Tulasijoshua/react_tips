df486b77e9f0a27d1300e7d0a6111f7dc29eb3ec
Tulasi Joshua Mbawini <mbawinitulasi12@gmail.com>
2024-02-26 22:46:07 +0000

    Feat: input

--- qrcode_generator/src/App.jsx
+++ qrcode_generator/src/App.jsx
@@ -1,12 +1,18 @@
 import { useState } from 'react'
 import './App.css'
+import InputForm from './components/InputForm'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className='text-4xl font-semibold'>
-      <div>Come around</div>
+    <div className='bg-gradient-to-r from-cyan-500 to-blue-500 h-screen pt-36 pb-2'>
+      <div className='container mx-auto max-w-4xl bg-white rounded-md shadow'>
+        <div className='md:grid md:grid-cols-3'>
+          <InputForm />
+          <h1>qrCode</h1>
+        </div>
+      </div>
     </div>
   )
 }
---
+++ qrcode_generator/src/components/InputForm.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import InputField from './InputField'
+
+const InputForm = () => {
+  return (
+    <div className='col-span-2 p-6 grid gap-4'>
+        <InputField />
+    </div>
+  )
+}
+
+export default InputForm
