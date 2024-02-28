74dacd2174e8c3ee0172ab97b1b1f9d298f2b1ca
Tulasi Joshua Mbawini <mbawinitulasi12@gmail.com>
2024-02-28 18:52:30 +0000

    Feat: generating qr code

--- qrcode_generator/src/components/QrCode.jsx
+++ qrcode_generator/src/components/QrCode.jsx
@@ -6,14 +6,32 @@ const QrCode = () => {
   const {response, loading, error} = useContext(InputContext);
   console.log(response)
 
+  if(loading) {
+    return (
+      <div className='animate-pulse flex flex-col items-center justify-center px-10 gap-3'>
+        <div className='h-32 w-full bg-gray-300'></div>
+        <div className='h-8 w-full bg-gray-300'></div>
+      </div>
+    )
+  }
+
+  if(error) {
+    return <div className='text-gray-500 flex items-center'>Sorry, Something wen wrong</div>
+  }
+
   return (
     <div className='bg-gray-100 rounded-r-md flex flex-col items-center justify-center'>
-        <div>
-            <img className='w-48' src={QRCode} alt="" />
-            <button
-                className='bg-blue-400 text-white mt-2 px-4 py-1 w-full'
-            >Download</button>
-        </div>
+      {response ? (
+      <div>
+          <img className='w-48' src={response} alt="" />
+          <button
+              className='bg-blue-400 text-white mt-2 px-4 py-1 w-full'
+          >Download</button>
+      </div>
+      ) : (
+        <div className=''>Your QrCode will be showing here...</div>
+      )}
+        
     </div>
   )
 }
