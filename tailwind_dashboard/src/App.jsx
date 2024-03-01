a85267680f92973939af61c8b61f6484f609b4f0
Tulasi Joshua Mbawini <mbawinitulasi12@gmail.com>
2024-03-01 04:35:53 +0000

    Feat: main

--- tailwind_dashboard/src/App.jsx
+++ tailwind_dashboard/src/App.jsx
@@ -1,15 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
+import {Route, Routes, useNavigate} from "react-router-dom";
+import AdminDashboard from './pages/admin-dashboard/adminDashboard';
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const navigate  = useNavigate();
+  useEffect(() => {
+    navigate('/admin-dashboard/marketing')
+  }, [])
   return (
-    <>
-      <div>Hey there</div>
-    </>
+    <div className='h-screen w-full'>
+      <Routes>
+        <Route path='/admin-dashboard/:category' element={<AdminDashboard />} />
+      </Routes>
+    </div>
   )
 }
 
--- tailwind_dashboard/src/pages/admin-dashboard/adminDashboard.jsx
+++ tailwind_dashboard/src/pages/admin-dashboard/adminDashboard.jsx
@@ -1,8 +1,31 @@
 import React from 'react'
+import Navbar from '../../components/admin-navbar/navbar'
+import Sidebar from '../../components/admin-sidebar/sidebar'
+import { useParams } from 'react-router-dom'
+import { Data } from '../../dates/jummy'
+import Main from '../../components/admin-main/main'
+import Marketing from '../../components/admin-marketing/marketing'
 
 const AdminDashboard = () => {
+    const {category} = useParams()
+    let cat = Data.find((categ) => categ.url === parseInt(category))
   return (
-    <div>AdminDashboard</div>
+    <div className='bg-slate-50'>
+        <div>
+            <Navbar />
+            <div className='main max-w-[2300px] mt-[76px] flex flex-1 justify-between'>
+                <Sidebar />
+                <div className={`main overflow-auto w-full h-full z-10`}>
+                    {
+                        category === 'main' && <Main />
+                    }
+                    {
+                        category === 'marketing' && <Marketing />
+                    }
+                </div>
+            </div>
+        </div>
+    </div>
   )
 }
 
