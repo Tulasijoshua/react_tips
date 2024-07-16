0179878603a820070495df95aa6897d602c0c336
Tulasijoshua <mbawinitulasi12@gmail.com>
2024-07-16 18:38:01 +0000

    feat: progress bar

--- progress-bar/src/App.jsx
+++ progress-bar/src/App.jsx
@@ -2,13 +2,24 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import CircleProgressBar from './components/CircleProgressBar'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [percentage, setPercentage] = useState(35)
+
+  console.log(percentage)
 
   return (
     <div>
-      <input type="text" min="1" max="100" step="1" />
+      <CircleProgressBar percentage={percentage} circleWidth="200" />
+      <input 
+        type="range" 
+        min="1" max="100" 
+        step="1" 
+        value={percentage} 
+        className='progressInput'
+        onChange={(ev) => setPercentage(ev.target.value)} 
+      />
     </div>
   )
 }
