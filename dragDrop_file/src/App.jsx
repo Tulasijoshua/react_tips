08641085dabc9319da900dd8480a506b0cad2526
Tulasijoshua <mbawinitulasi12@gmail.com>
2024-08-18 23:18:53 +0000

    feat: drag drop coponent

--- dragDrop_file/src/App.jsx
+++ dragDrop_file/src/App.jsx
@@ -2,13 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import DragDropFile from './components/DragDropFile'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className='container'>
-      <h1>Hello world</h1>
+      <DragDropFile />
     </div>
   )
 }
--- dragDrop_file/src/components/DragDropFile.jsx
+++ dragDrop_file/src/components/DragDropFile.jsx
@@ -1,8 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const DragDropFile = () => {
+    const [files, setFiles] = useState();
+
+    const handleDragOver = (e) => {
+        e.preventDefault();
+        console.log(e)
+
+    }
+    const handleDrop = (e) => {
+        e.preventDefault();
+        console.log(e.dataTransfer.files)
+    }
   return (
-    <div>DragDropFile</div>
+    <div>
+        {!files && (
+            <div 
+                className='dropzone'
+                onDragOver={handleDragOver}
+                onDrop={handleDrop}
+            >
+                <h1>Drag and Drop files to Upload</h1>
+                <h1>Or</h1>
+                <input 
+                    type="file" 
+                    multiple
+                    onChange={e => setFiles(e.target.value)}
+                    hidden
+                    ref={}
+                />
+                <button>Select Files</button>
+            </div>
+        )}
+    </div>
   )
 }
 
